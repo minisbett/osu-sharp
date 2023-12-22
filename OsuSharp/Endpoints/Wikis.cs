b573de296d78048fa61d812d0dc12c6e8b0f9881
minisbett <39670899+minisbett@users.noreply.github.com>
2023-12-22 14:39:12 +0100

    Add wiki pages

---
+++ OsuSharp/Endpoints/Wikis.cs
@@ -0,0 +1,29 @@
+﻿using OsuSharp.Models.Wikis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuSharp;
+
+public partial class OsuApiClient
+{
+  // API docs: https://osu.ppy.sh/docs/index.html#wiki
+
+  /// <summary>
+  /// Gets the wiki page at the specified path in the specified locale.
+  /// Returns null if the wiki page was not found.
+  /// <br/><br/>
+  /// API notes:
+  /// <a href="https://osu.ppy.sh/docs/index.html#get-wiki-page"/>
+  /// </summary>
+  /// <param name="locale">The locale, as a 2-letter language code.</param>
+  /// <param name="path">The path of the wiki page.</param>
+  /// <returns>The wiki page or null, if the wiki page was not found.</returns>
+  public async Task<WikiPage?> GetWikiPageAsync(string locale, string path)
+  {
+    // Send the request and return the wiki page object.
+    return await GetFromJsonAsync<WikiPage>($"wiki/{locale}/{path}");
+  }
+}
---
+++ OsuSharp/Enums/WikiPageLayout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OsuSharp.Models.Wikis;
+
+namespace OsuSharp.Enums;
+
+/// <summary>
+/// An enum containing the types of layout of a <see cref="WikiPage"/>.
+/// <br/><br/>
+/// API docs: Not documented, refer to source<br/>
+/// Source: <a href="https://github.com/ppy/osu-web/blob/master/app/Models/Wiki/Page.php"/>
+/// </summary>
+public enum WikiPageLayoutType
+{
+  /// <summary>
+  /// Indicates that the wiki page is a normal page written in Markdown.
+  /// </summary>
+  [Description("markdown_page")]
+  Markdown,
+
+  /// <summary>
+  /// Indicates that the wiki page is a main page, having a special layout.
+  /// </summary>
+  [Description("main_page")]
+  Main
+}
